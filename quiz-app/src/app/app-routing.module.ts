9fe75fe422b2cbed1361b4a780a994d7069f43dc
Konstantinos <kongallis@gmail.com>
2020-11-18 14:15:55 +0200

    Generated components and created routes

--- quiz-app/src/app/app-routing.module.ts
+++ quiz-app/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
+import { RegisterComponent } from './register/register.component';
+import { ResultComponent } from './result/result.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [   
+{path: "register", component: RegisterComponent},
+{path: "quiz", component: QuizComponent},
+{path: "result", component: ResultComponent},
+{path: "", redirectTo:"/register", pathMatch: "full"},];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
